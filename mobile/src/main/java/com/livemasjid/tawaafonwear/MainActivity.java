d2ca0ca06490ec2f6b5a88aea34e9086b7c87300
Yusuf Kaka <yusufk@gmail.com>
2026-02-08 08:43:14 +0200

    Updated to SDK 36

--- mobile/src/main/java/com/livemasjid/tawaafonwear/MainActivity.java
+++ mobile/src/main/java/com/livemasjid/tawaafonwear/MainActivity.java
@@ -1,17 +1,16 @@
 package com.livemasjid.tawaafonwear;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.loadUrl("file:///android_asset/main.html");
+        WebView webView = findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/main.html");
     }
 }
